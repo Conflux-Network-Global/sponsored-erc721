559dbf46581a2c0d15e95b96e1f5106818d2b8a4
aalu1418 <aalu1418@gmail.com>
2020-11-06 17:44:12 -0500

    formatting

--- golang/nft.go
+++ golang/nft.go
@@ -1,12 +1,12 @@
 package nft
 
 import (
+	"fmt"
 	conflux "github.com/Conflux-Chain/go-conflux-sdk"
 	"github.com/Conflux-Chain/go-conflux-sdk/types"
 	"github.com/joho/godotenv"
+	"math/big"
 	"os"
-  "math/big"
-  "fmt"
 )
 
 type NFT struct {
@@ -31,19 +31,18 @@ func New(url string, addr string, passphrase string) (NFT, error) {
 
 	//set up account
 	account := conflux.NewAccountManager("./keystore")
-  _, err = account.GetDefault() //get default account
+	_, err = account.GetDefault() //get default account
 	if err != nil {
-    //fallback to importing key if ./keystore doesn't exist
-    _, err = account.ImportKey(os.Getenv("PRIVATE_KEY"), passphrase)
-    if err != nil {
-      return NFT{}, err
-    }
+		//fallback to importing key if ./keystore doesn't exist
+		_, err = account.ImportKey(os.Getenv("PRIVATE_KEY"), passphrase)
+		if err != nil {
+			return NFT{}, err
+		}
 	}
-  err = account.UnlockDefault(passphrase)
-  if err != nil {
-    return NFT{}, err
-  }
-
+	err = account.UnlockDefault(passphrase)
+	if err != nil {
+		return NFT{}, err
+	}
 
 	client.SetAccountManager(account)
 	a := types.Address(addr)
@@ -61,36 +60,36 @@ func New(url string, addr string, passphrase string) (NFT, error) {
 }
 
 func (nft NFT) Mint(addr string, id string) (*types.Hash, error) {
-  a := types.Address(addr)
-  i := new(big.Int)
-  i, ok := i.SetString(id, 10)
-  if !ok {
-    null := types.Hash("")
-    return &null, fmt.Errorf("Invalid ID")
-  }
+	a := types.Address(addr)
+	i := new(big.Int)
+	i, ok := i.SetString(id, 10)
+	if !ok {
+		null := types.Hash("")
+		return &null, fmt.Errorf("Invalid ID")
+	}
 
-  txhash, err := nft.Contract.SendTransaction(nil, "safeMint", a.ToCommonAddress(), i)
-  return txhash, err
+	txhash, err := nft.Contract.SendTransaction(nil, "safeMint", a.ToCommonAddress(), i)
+	return txhash, err
 }
 
 func (nft NFT) GetAssets(addr string) ([]big.Int, error) {
-  a := types.Address(addr)
+	a := types.Address(addr)
 
-  balance := &big.Int{}
-  err := nft.Contract.Call(nil, &balance, "balanceOf", a.ToCommonAddress())
-  if err != nil {
-    return []big.Int{}, err
-  }
+	balance := &big.Int{}
+	err := nft.Contract.Call(nil, &balance, "balanceOf", a.ToCommonAddress())
+	if err != nil {
+		return []big.Int{}, err
+	}
 
-  ids := make([]big.Int, balance.Int64())
-  for i:= 0; int64(i) < balance.Int64(); i++ {
-    id := &big.Int{}
-    err := nft.Contract.Call(nil, &id, "tokenOfOwnerByIndex", a.ToCommonAddress(), big.NewInt(int64(i)))
-    if err != nil {
-      return []big.Int{}, err
-    }
-    ids[i] = *id
-  }
+	ids := make([]big.Int, balance.Int64())
+	for i := 0; int64(i) < balance.Int64(); i++ {
+		id := &big.Int{}
+		err := nft.Contract.Call(nil, &id, "tokenOfOwnerByIndex", a.ToCommonAddress(), big.NewInt(int64(i)))
+		if err != nil {
+			return []big.Int{}, err
+		}
+		ids[i] = *id
+	}
 
-  return ids, err
+	return ids, err
 }
